5ea3f2d14dca5461ecbfeacc8e3d46cf0980cd21
Ubuntu <ubuntu@ip-10-204-38-200.ec2.internal>
2013-07-12 05:05:17 +0000

    segunda alteracao

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var content = new Buffer(fs.readFileSync("index.html")).toString();
+	response.send(content);
 });
 
 var port = process.env.PORT || 5000;
